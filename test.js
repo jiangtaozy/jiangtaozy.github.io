1bce4ec92ef352b8ada9ea51b9d26a9bc7f9fe5f
Jemo <1321332562@qq.com>
2017-08-23 20:47:46 +0800

    add test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var contents = fs.readFileSync('./res/hacker-book/contents.txt').toString();
+// console.log('contents: ', contents);
+var contentsArray = contents.split('\n');
+// console.log('contentsArray: ', contentsArray);
+while(contentsArray[contentsArray.length - 1].length == 0) {
+  contentsArray.pop();
+}
+console.log('contentsArray.length: ', contentsArray.length);  
+var count = 0;
+function compare(str1, str2) {
+  console.log(str1, '.indexOf(\'-\'): ', str1.indexOf('-'));
+  var s1 = str1.substring(0, str1.indexOf('-'));
+  var s2 = str2.substring(0, str2.indexOf('-'));
+  console.log(s1, ':', s2, '=', s1.localeCompare(s2), ' ', count++);
+  return s1.localeCompare(s2);
+}
+contentsArray.sort(compare);
+console.log('contentsArray: ', contentsArray);  
+
