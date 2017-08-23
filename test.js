b021cdd2682d7e2792b61da524aa3718b4572f4c
Jemo <1321332562@qq.com>
2017-08-23 22:01:38 +0800

    add zingtouch.min.js

--- test.js
+++ test.js
@@ -6,13 +6,13 @@ var contentsArray = contents.split('\n');
 while(contentsArray[contentsArray.length - 1].length == 0) {
   contentsArray.pop();
 }
-console.log('contentsArray.length: ', contentsArray.length);  
+// console.log('contentsArray.length: ', contentsArray.length);  
 var count = 0;
 function compare(str1, str2) {
-  console.log(str1, '.indexOf(\'-\'): ', str1.indexOf('-'));
+  // console.log(str1, '.indexOf(\'-\'): ', str1.indexOf('-'));
   var s1 = str1.substring(0, str1.indexOf('-'));
   var s2 = str2.substring(0, str2.indexOf('-'));
-  console.log(s1, ':', s2, '=', s1.localeCompare(s2), ' ', count++);
+  // console.log(s1, ':', s2, '=', s1.localeCompare(s2), ' ', count++);
   return s1.localeCompare(s2);
 }
 contentsArray.sort(compare);
